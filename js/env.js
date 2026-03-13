cb38a37da9c4d223b896f04738035f34b2bdf262
Vidish Kumar <vidishkumar890@gmail.com>
2026-03-13 11:01:27 +0530

    delete

--- js/env.js
+++
@@ -1,5 +0,0 @@
-// Public runtime config for deployed environments.
-// OAuth Client ID is safe to expose in frontend apps.
-window.GW_ENV = {
-  GOOGLE_CLIENT_ID: '232211253749-329g45udbr3issn5qul8ki14c4e9kl3d.apps.googleusercontent.com'
-};
